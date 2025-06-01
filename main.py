cd0ae71e0dc26405e4018e52485a32b64644e1fc
rod-ml <rml317@icloud.com>
2025-05-31 21:32:56 -0500

    created circleshape class for asteroid sprites and created a player sprite
    Player sprite is a child of the CircleShape class. This is for the
    hitbox of the player, even though the player is rendered as a triangle.

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import pygame
 from constants import *
-
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+from player import *
 
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
