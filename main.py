6e90a681a7ae594c182e367605a7e70f17374bf3
rod-ml <rml317@icloud.com>
2025-05-31 22:26:32 -0500

    added rotation functionality for player

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill("black")
-        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
         player.draw(screen)
         pygame.display.flip()
 
