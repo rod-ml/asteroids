d463029adea67d47c3c9e19265a1c9bff86ddacf
rod-ml <rml317@icloud.com>
2025-06-01 19:51:15 -0500

    added the ability to shoot, destroy asteroids, and split asteroids upon shot collision

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
@@ -14,9 +15,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
 
@@ -32,6 +36,11 @@ def main():
             if player.check_collision(asteroid):
                 print("Game over!")
                 sys.exit()
+        for shot in shots:
+            for asteroid in asteroids:
+                if shot.check_collision(asteroid):
+                    asteroid.split()
+                    shot.kill()
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
